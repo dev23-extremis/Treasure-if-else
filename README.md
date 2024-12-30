6987e2e54a434f1fc8a8a93d1bf62a08507fba54
devt40363@gmail.com <devt40363@gmail.com>
2024-12-30 21:08:50 +0530

    Initial commit on 2024-12-30 21:08:50

---
+++ README.md
@@ -0,0 +1,3 @@
+# Treasure-if-else
+
+Today i learned the used of if else and created a game called treasure.py
---
+++ test.py
@@ -0,0 +1,125 @@
+# print("Welcome to the rollercoster!")
+# height = int(input("Enter your height in cm: "))
+
+# if height >= 120:
+#     print("You cna ride")
+
+# else:
+#     print("You can't ride")
+
+
+# number = int(input("Enter a number: "))
+
+# if number % 2 == 0:
+#     print("This is an even number")
+
+# else:
+#     print("This is a ood number")
+
+# print("Welcome to the rollercoster!")
+# height = int(input("Enter your height in cm: "))
+
+# if height >= 120:
+#     print("You can ride")
+#     age = int(input("Enter your age: "))
+#     if age < 12:
+#         print("Please pay $5")
+#     elif age <= 18:
+#         print("Please pay $7")
+#     else:
+#         print("Please pay $12")
+
+# else:
+#     print("You can't ride")
+
+# print("Welcome to the rollercoster!")
+# height = int(input("Enter your height in cm: "))
+
+# if height >= 120:
+#     print("You can ride")
+    
+#     age = int(input("Enter your age: "))
+#     if age < 12:
+#         price = 5
+#         print(f"Ride's price ${price}")
+#     elif age <= 18:
+#         price = 7
+#         print(f"Ride's price ${price}")
+#     else:
+#         price = 12
+#         print(f"Ride's price ${price}")
+    
+#     choice = input("Do you want to take photos? (Y/N): ")
+#     if choice =='N':
+#         print(f"The total bill including ride is ${price}")
+#     else:
+#         price +=3
+#         print(f"The total bill including ride is ${price}")
+
+# else:
+#     print("You can't ride")
+
+# print("Welcome to the pizza delivery!")
+# size = input("Enter the size of the pizza (S/M/L): ")
+# pepperoni = input("Do you want pepperoni? (Y/N): ")
+# extra_cheese = input("Do you want extra cheese? (Y/N): ")
+
+# price = 0
+
+# if size == 'S':
+#     price = 15
+#     if pepperoni == 'Y':
+#         price +=2
+#     else:
+#         price
+
+# elif size == 'M':
+#     price = 20
+#     if pepperoni == 'Y':
+#         price +=3
+#     else:
+#         price
+
+# elif size == 'L':
+#     price = 25
+#     if pepperoni == 'Y':
+#         price +=3
+#     else:
+#         price
+
+# if extra_cheese == 'Y':
+#         price +=1
+# else:
+#     price
+
+# print(f"Your order comes out to be of ${price}")
+
+# print("Welcome to the rollercoster!")
+# height = int(input("Enter your height in cm: "))
+
+# if height >= 120:
+#     print("You can ride")
+    
+#     age = int(input("Enter your age: "))
+#     if age < 12:
+#         price = 5
+#         print(f"Ride's price ${price}")
+#     elif age < 18:
+#         price = 7
+#         print(f"Ride's price ${price}")
+#     elif age > 18 and age < 45:
+#         price = 12
+#         print(f"Ride's price ${price}")
+#     elif age >= 45 and age <=55:
+#         price = 0
+#         print(f"Ride's price ${price}")
+    
+#     choice = input("Do you want to take photos? (Y/N): ")
+#     if choice =='N':
+#         print(f"The total bill including ride is ${price}")
+#     else:
+#         price +=3
+#         print(f"The total bill including ride is ${price}")
+
+# else:
+#     print("You can't ride")
